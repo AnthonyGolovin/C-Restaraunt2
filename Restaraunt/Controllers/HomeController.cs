c28aeb535c03359e0004cc46dd5d48d7f9b4b02b
Anthony Golovingit config --global user.name Anthony <AGOLOVIN97@live.com>
2020-01-16 18:00:20 -0800

    Fully convert Restaurant variable names to reflect their model counter parts

--- Restaraunt/Controllers/HomeController.cs
+++ Restaraunt/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace ToDoList.Controllers
+namespace   Food.Controllers
 {
   public class HomeController : Controller
   {
--- Restaraunt/Controllers/RestaurantsController.cs
+++ Restaraunt/Controllers/RestaurantsController.cs
@@ -1,50 +1,50 @@
 using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
+using Food.Models;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace ToDoList.Controllers
+namespace Food.Controllers
 {
-  public class ItemsController : Controller
+  public class RestaurantsController : Controller
   {
-    private readonly ToDoListContext _db;
+    private readonly FoodContext _db;
 
-    public ItemsController(ToDoListContext db)
+    public RestaurantsController(FoodContext db)
     {
       _db = db;
     }
 
     public ActionResult Index()
     {
-      List<Item> model = _db.Items.ToList();
+      List<Restaurant> model = _db.Restaurants.ToList();
       return View(model);
     }
     public ActionResult Create()
 {
-    ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name");
+    ViewBag.CuisineId = new SelectList(db.Cuisines, "CuisineId", "Name");
     return View();
 }
 
     [HttpPost]
-    public ActionResult Create(Item item)
+    public ActionResult Create(Restaurant restaurant)
     {
-      _db.Items.Add(item);
+      _db.Restaurants.Add(restaurant);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
     public ActionResult Details(int id)
     {
-    Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
-    return View(thisItem);
+    Restaurant thisRestaurant = _db.Restaurant.FirstOrDefault(restaurant => restaurants.RestaurantId == id);
+    return View(thisRestaurant);
     }
     public ActionResult Edit(int id)
     {
-    var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
-    ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name");
-    return View(thisItem);
+    var thisRestaurant = db.Restaurants.FirstOrDefault(restaurant => restaurants.RestaurantId == id);
+    ViewBag.CuisineId = new SelectList(db.Cuisines, "CuisineId", "Name");
+    return View(thisRestaurant);
     }
     [HttpPost]
-    public ActionResult Edit(Item item)
+    public ActionResult Edit(Restaurant restaurant)
     {
         _db.Entry(item).State = EntityState.Modified;
         _db.SaveChanges();
@@ -53,15 +53,15 @@ namespace ToDoList.Controllers
 
     public ActionResult Delete(int id)
     {
-        var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
-        return View(thisItem);
+        var thisRestaurant = _db.Restaurant.FirstOrDefault(restaurant => restaurants.RestaurantId == id);
+        return View(thisRestaurant);
     }
 
     [HttpPost, ActionName("Delete")]
     public ActionResult DeleteConfirmed(int id)
     {
-        var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
-        _db.Items.Remove(thisItem);
+        var thisRestaurant = _db.Restaurant.FirstOrDefault(restaurants => restaurants.RestaurantId == id);
+        _db.Restaurant.Remove(thisRestaurant);
         _db.SaveChanges();
         return RedirectToAction("Index");
     }
